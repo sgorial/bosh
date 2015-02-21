2fe1afdb518b5f73cae402657ad6f883ae0ae9e0
Simon Gorial <sgorial@users.noreply.github.com>
2015-02-21 15:34:19 -0500

    Update vpc.rb

--- bosh_cli_plugin_aws/lib/bosh_cli_plugin_aws/vpc.rb
+++ bosh_cli_plugin_aws/lib/bosh_cli_plugin_aws/vpc.rb
@@ -2,7 +2,7 @@ module Bosh
   module Aws
     class VPC
 
-      DEFAULT_CIDR = "10.96.32.0/22"
+      DEFAULT_CIDR = "10.96.32.0/20"
       DEFAULT_ROUTE = "0.0.0.0/0"
       NAT_INSTANCE_DEFAULTS = {
           :image_id => "ami-f619c29f",
